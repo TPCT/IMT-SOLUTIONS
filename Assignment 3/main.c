4d9338a3135e81ef51e74801c9ae74ed709dd6c2
TPCT <eslamjackie@outlook.com>
2019-07-10 22:52:55 +0200

    Create main.c

---
+++ Assignment 3/main.c
@@ -0,0 +1,11 @@
+#include "assignment 2.c"
+
+int main() {
+    int numbers[10], stp_index = 0;
+    stp_index = ScanFunction(numbers, 10);
+    int max_number = Maximium(numbers, stp_index);
+    int min_number = Minimium(numbers, stp_index);
+    printf("Max: %d, Min: %d", max_number, min_number);
+    getchar();
+    return 0;
+}
