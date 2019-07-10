440a1f4aad90a48d39e90dc51bd920402011b7b1
TPCT <eslamjackie@outlook.com>
2019-07-10 23:12:29 +0200

    Update Assignment 4.c

--- Assignment 4/Assignment 4.c
+++ Assignment 4/Assignment 4.c
@@ -1,4 +1,4 @@
-#include "Assignment 4.h"
+#include "Assignment 2.h"
 
 typedef struct{
         unsigned char Operation_Select: 4;
@@ -7,8 +7,9 @@ typedef struct{
         unsigned char OperandA : 1;
         unsigned char OperandB : 1;
         unsigned char dot_in : 1;
-        unsigned char OperandA_Counter: 64;
-        unsigned char OperandB_Counter: 64;
+        unsigned long long OperandA_Counter: 64;
+        unsigned long long OperandB_Counter: 64;
+        unsigned long long dot_counter : 1;
 } Selector;
 
 double Operation_Doer(double operandA, double operandB, char operation){
@@ -43,8 +44,8 @@ void Calculator(void){
     Selector select;
     select.OperandB = select.OperandA = select.Operation_Select
                     = select.Operation_Counter = select.Operation_Start
-                    = select.dot_in = select.OperandB_Counter = select.OperandA_Counter = 0;
-    unsigned char dot_counter = 1;
+                    = select.dot_in = select.OperandB_Counter = select.OperandA_Counter  = 0;
+    select.dot_counter = 1;
     char operation[10] = {0}, c;
     double operandA = 0, operandB = 0;
     void cleaner(void){
@@ -76,13 +77,13 @@ void Calculator(void){
                     printf("The Result Of Operation: %lf", Operation_Doer(operandA, 0, select.Operation_Select));
                     return;
                 }
-                operandA /= dot_counter;
-                dot_counter = 1;
+                operandA /= select.dot_counter;
+                select.dot_counter = 1;
                 select.OperandA = 1;
                 select.dot_in = 0;
                 printf("Please enter Operand B: ");
             }else if(select.OperandB_Counter){
-                operandB /= dot_counter;
+                operandB /= select.dot_counter;
                 printf("The Result Of Operation: %f", Operation_Doer(operandA, operandB, select.Operation_Select));
                 return;
             }
@@ -92,11 +93,11 @@ void Calculator(void){
         }else if(c == '.'){
             select.dot_in = 1;
         }else if(!select.OperandA && isdigit(c)){
-            if (select.dot_in){dot_counter *= 10;}
+            if (select.dot_in){select.dot_counter *= 10;}
             operandA = operandA * 10 + (c - '0');
             select.OperandA_Counter += 1;
         }else if(!select.OperandB && isdigit(c)){
-            if (select.dot_in){dot_counter *= 10;}
+            if (select.dot_in){select.dot_counter *= 10;}
             operandB = operandB * 10 + (c - '0');
             select.OperandB_Counter += 1;
         }else if(c == 8){
